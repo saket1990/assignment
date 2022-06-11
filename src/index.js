eeee4310bf8a5973cbeee8d62f8b00b8518e70c9
saket1990 <saketkumarsunny@gmail.com>
2022-06-11 22:48:28 +0530

    assignment

--- src/index.js
+++ src/index.js
@@ -1,26 +1,43 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const route = require('./routes/route.js');
-const { default: mongoose } = require('mongoose');
+//const { default: mongoose } = require('mongoose');
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-mongoose.connect("mongodb+srv://functionup-cohort:G0Loxqc9wFEGyEeJ@cluster0.rzotr.mongodb.net/Pritesh8769811-DB?retryWrites=true&w=majority", {
+mongoose.connect("mongodb+srv://rhutvik-patel:jiCI0diV4CDbN9Pr@cluster0.afbog.mongodb.net/saketDB", {
     useNewUrlParser: true
 })
 .then( () => console.log("MongoDb is connected"))
 .catch ( err => console.log(err) )
 
-app.use (
-    function (req, res, next) {
-        console.log ("inside GLOBAL MW");
-        next();
-  }
-  );
+// app.use (
+//     function (req, res, next) {
+//         console.log ("inside GLOBAL MW");
+//         next();
+//   }
+//   );
 
+
+
+const middleware= function(req, res, next){
+        let currentDate=new Date()
+        let a=currentDate.getDate()
+        let b=currentDate.getMonth()+1
+        let c=currentDate.getFullYear()
+        let e=currentDate.getHours()
+        let f=currentDate.getMinutes()
+        let g=currentDate.getSeconds()
+        let ip=req.ip
+        let url=req.url
+        console.log(a,"-",b,"-",c,e,":",f,":",g,ip,url)
+        next()
+    }
+app.use(middleware);
 app.use('/', route);
 
 
--- src/routes/route.js
+++ src/routes/route.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-// const UserModel= require("../models/userModel.js")
+const UserModel= require("../models/userModel.js")
 const UserController= require("../controllers/userController")
 const BookController= require("../controllers/bookController")
 const commonMW = require ("../middlewares/commonMiddlewares")
